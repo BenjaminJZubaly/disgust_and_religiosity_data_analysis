2c7daddf4959fb7f20f8ef736eed0c0ad4581205
Benjamin Zubaly <benjaminjzubaly@gmail.com>
2024-06-24 19:06:52 -0700

    clean basic demographics (not eth nat rel)

--- code/data_cleaning.R
+++ code/data_cleaning.R
@@ -14,3 +14,59 @@ suppressMessages(groundhog.library(pkg = pkg, date = "2024-06-01"))
 
 # Reading in the unclean data as data.unclean
 data.unclean <- read.csv(file = "./data/prepared_data.csv")
+
+# Format data types
+  # IDs as strings
+data.unclean$ID <- as.character(data.unclean$ID)
+  # Start and finish time as date format
+data.unclean <- data.unclean %>%
+  mutate(start_time = parse_date_time(start_time, orders = "mdy HM", tz = "UTC")) %>%
+  mutate(start_time = as.POSIXct(start_time, origin = "1970-01-01", tz = "UTC"))
+data.unclean <- data.unclean %>%
+  mutate(finish_time = parse_date_time(finish_time, orders = "mdy HM", tz = "UTC")) %>%
+  mutate(finish_time = as.POSIXct(finish_time, origin = "1970-01-01", tz = "UTC"))
+
+# Calculate age (drop down started at 18)
+data.unclean$age <- data.unclean$age + 17
+
+# Code sex variable as factor
+  # Make them the right numbers
+data.unclean$sex <- data.unclean$sex - 1
+  # Make it a factor
+data.unclean$sex <- factor(data.unclean$sex, 
+                           levels = c("0", "1"),
+                           labels = c("Male", "Female"))
+
+# Turning gender responses into standardized labels
+data.unclean <- data.unclean %>%
+  mutate(
+    gender = str_trim(gender),  # Remove leading and trailing spaces
+    gender = str_to_lower(gender),  # Convert all to lowercase
+    gender = case_when(
+      gender %in% c("male", "man", "masculine", "xy-male", "boy", "men", "answered above already") ~ "Male",
+      gender %in% c("female", "woman", "she/her") ~ "Female",
+      gender %in% c("non-binary", "non binary", "nonbinary") ~ "Non-binary",
+      gender %in% c("non-binary (agender)", "agender") ~ "Agender",
+      gender %in% c("gender fluid") ~ "Gender Fluid",
+      gender %in% c("queer") ~ "Gender Queer",
+      TRUE ~ NA_character_  # Assign NA to any unclassified responses
+    )
+  )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
